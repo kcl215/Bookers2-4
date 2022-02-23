6ef3632608130bdcc1f1eef30324efdef27fe580
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2022-02-23 11:08:18 +0000

    [ADD]アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,26 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  # ＝＝＝＝＝＝＝＝＝＝＝＝＝自分がフォローしているユーザーとの関連＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # Relationshipモデル参照からfollower_id（フォローする側）カラム指定。
+  # フォローする側のUserから見て、フォローされる側のUserを集める。
+  has_many :followings, through: :relationships, source: :followed
+  # Relationshipモデル参照しfollowed（フォローされた側）のユーザー情報を集めることを「followings」と定義
+　#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝ 　
+
+
+  # ＝＝＝＝＝＝＝＝＝＝＝＝＝自分がフォローされるユーザーとの関連＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # Relationshipモデル参照からfollowed_id（フォローされる側）カラム指定。
+  # フォローされる側のUserから見て、フォローしてくる側のUserを集める。
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # Relationshipモデル参照しfollower（フォローする側）のユーザー情報を集めることを「followers」と定義
+  #＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝ 　
+  
+    
+    
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
